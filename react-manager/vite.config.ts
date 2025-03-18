9884236403a932df8f1922aa7844416835223c18
Finch Chen <1004753438@qq.com>
2025-03-18 09:02:54 +0800

    feat: 配置server和resolve

--- react-manager/vite.config.ts
+++ react-manager/vite.config.ts
@@ -1,13 +1,20 @@
-/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://vite.dev/config/
 import path from 'path';
 export default defineConfig({
-	plugins: [react()],
+	//服务器配置
+	server: {
+		host: '127.0.0.1',
+		port: 8080,
+		proxy: {
+			'/api': 'http://api-driver.marsview.cc',
+		},
+	},
+	//别名路径配置
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, '/src'),
 		},
 	},
+	plugins: [react()],
 });
