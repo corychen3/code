cbbce7f5806f69296f2f5ad1b1e1a02a235a0b35
Finch Chen <1004753438@qq.com>
2025-03-22 10:34:05 +0800

    feat: 请求和相应拦截器

--- react-manager/src/utils/request.ts
+++ react-manager/src/utils/request.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import { message } from 'antd';
+import axios, { AxiosError } from 'axios';
 
 const instance = axios.create({
 	baseURL: '/api',
@@ -7,12 +8,37 @@ const instance = axios.create({
 	withCredentials: true,
 });
 
+instance.interceptors.request.use(
+	config => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			config.headers.Authorization = 'Token::' + token;
+		}
+		return { ...config };
+	},
+	(error: AxiosError) => {
+		return Promise.reject(error);
+	},
+);
+
+instance.interceptors.response.use(res => {
+	const data = res.data;
+	if (data.code === 500001) {
+		message.error(data.msg);
+		localStorage.removeItem('token');
+		location.href = '/login';
+	} else if (data.code != 0) {
+		return Promise.reject(data);
+	}
+	return data.data;
+});
+
 export default {
 	get(url: string, params: any) {
-		instance.get(url, { params });
+		return instance.get(url, { params });
 	},
 
 	post(url: string, params: any) {
-		instance.post(url, params);
+		return instance.post(url, params);
 	},
 };
