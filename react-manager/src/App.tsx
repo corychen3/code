1b622f6fbb91856e891963e78d05fd95ad86796b
Finch Chen <1004753438@qq.com>
2025-04-08 20:10:38 +0800

    feat:antd主题定制

--- react-manager/src/App.tsx
+++ react-manager/src/App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-import './App.less';
 /*
  * @Author: Finch
  * @Date: 2025-04-08 18:54:41
- * @LastEditTime: 2025-04-08 19:00:40
+ * @LastEditTime: 2025-04-08 20:07:23
  * @LastEditors: Finch
  * @Description:
  * @FilePath: \code\react-manager\src\App.tsx
  * 可以输入预定的版权声明、个性签名、空行等
  */
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { ConfigProvider } from 'antd';
+import './App.less';
 const App: React.FC = () => {
 	return (
 		<div>
-			<RouterProvider router={router} />
+			<ConfigProvider
+				theme={{
+					token: {
+						colorPrimary: '#cb7250',
+					},
+				}}
+			>
+				<RouterProvider router={router} />
+			</ConfigProvider>
 		</div>
 	);
 };
